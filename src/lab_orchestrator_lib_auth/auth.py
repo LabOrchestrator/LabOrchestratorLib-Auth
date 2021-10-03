fc38842438baf69b9f6f88aceae02a406e0bc2c1
niwla23 <alwin@cloudserver.click>
2021-10-03 19:03:40 +0200

    return the instance, not the class of TokenParams

--- src/lab_orchestrator_lib_auth/auth.py
+++ src/lab_orchestrator_lib_auth/auth.py
@@ -82,4 +82,4 @@ def verify_auth_token(token: str, vmi_name: str, secret_key: str) -> Tuple[bool,
     data = decode_auth_token(token, secret_key)
     is_forbidden = data is None or vmi_name not in data.allowed_vmi_names
 
-    return not is_forbidden, LabInstanceTokenParams
+    return not is_forbidden, data
